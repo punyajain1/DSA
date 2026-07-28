69fbe5906c52f713d359534a2cd100fe0c870cad
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-07-28 15:22:06 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans="";
+        for(auto it:strs){
+            ans += to_string(it.size()) + "#" + it;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        int n=s.size();
+        vector<string> ans;
+        string temp="";
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while (s[j] != '#'){
+                j++;
+            }
+            int len=stoi(s.substr(i, j-i));
+            ans.push_back(s.substr(j+1,len));
+            i = j+1+len;
+        }
+        return ans;
+    }
+};
