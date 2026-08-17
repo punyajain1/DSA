7ac41b84149852222472616ec19a87f40687ba78
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-08-17 18:03:47 +0530

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int n:nums){
+            int i=abs(n)-1;
+            if(nums[i]<0){
+                return abs(n);
+            }
+            nums[i]*=-1;
+        }
+        return -1;
+    }
+};
