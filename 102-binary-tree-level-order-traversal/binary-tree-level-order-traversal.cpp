9bdaccd66576b276ae686cb12351e5829177fb75
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-08-20 17:55:55 +0530

    Time: 4 ms (12.70%) | Memory: 17.8 MB (5.29%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -11,32 +11,51 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root==NULL) return{};
-        queue<TreeNode*> q;
-        q.push(root);
-        q.push(NULL);
-        vector<vector<int>> ans;
-        vector<int> v;
-        while(!q.empty()){
-            TreeNode* t=q.front();
-            q.pop();
-            if(t==NULL){
-                ans.push_back(v);
-                v.clear();
-                if(!q.empty()){
-                    q.push(NULL);
-                }
-            }else{
-                v.push_back(t->val);
-                if(t->left){
-                    q.push(t->left);
-                }
-                if(t->right){
-                    q.push(t->right);
-                }
-            }
+    vector<vector<int>>ans;
+    void solve(TreeNode* root,int level){
+        if(!root) return;
+        if(ans.size()==level){
+            ans.push_back(vector<int>());
         }
+        ans[level].push_back(root->val);
+        solve(root->left,level+1);
+        solve(root->right,level+1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        solve(root,0);
         return ans;
     }
+
+
+
+    //BFS->
+    
+    // vector<vector<int>> levelOrder(TreeNode* root) {
+    //     if(root==NULL) return{};
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     q.push(NULL);
+    //     vector<vector<int>> ans;
+    //     vector<int> v;
+    //     while(!q.empty()){
+    //         TreeNode* t=q.front();
+    //         q.pop();
+    //         if(t==NULL){
+    //             ans.push_back(v);
+    //             v.clear();
+    //             if(!q.empty()){
+    //                 q.push(NULL);
+    //             }
+    //         }else{
+    //             v.push_back(t->val);
+    //             if(t->left){
+    //                 q.push(t->left);
+    //             }
+    //             if(t->right){
+    //                 q.push(t->right);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
