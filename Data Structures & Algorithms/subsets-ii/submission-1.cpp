1a4310f9b993edf6a0f90ed0cf80f6b98a3b8f08
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-09-04 12:11:21 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> v;
+    void sub(int i, vector<int> &ans, vector<int> &nums) {
+        v.push_back(ans);
+        for (int j = i; j < nums.size(); j++){
+            if (j > i && nums[j]==nums[j-1]) continue; // skip duplicate sets as is there is a copy number then it wont gentrate previously genrated set
+            ans.push_back(nums[j]); //push current in ans
+            sub(j + 1, ans, nums); // moving to next recursion
+            ans.pop_back(); //poping the pushed above 
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        sub(0,ans,nums);
+        return v;
+    }
+};
