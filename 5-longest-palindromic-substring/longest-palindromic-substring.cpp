8996fffc8ac49bedaa947085739e59ebb118ad06
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-09-25 20:59:29 +0530

    Time: 90 ms (28.80%) | Memory: 26 MB (27.73%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int l=0;
-        int r=-1;
-        string t = "#";
-        for (char c:s) {
-            t += c;
-            t += "#";
-        }
-        int n=t.size();
-        int c=0,ml=0;
-        vector<int> p(n);
-        for(int i=0;i<n;i++){
-            int k;
-            if(i>r){
-                k=0;
-            }else{
-                int j=r-i+l;
-                k=min(p[j],r-i);
-            }
-            while(i-k-1>=0 && i+k+1<n && t[i-k-1]==t[i+k+1]){
-                k++;
-            }
-            p[i]=k;
-            if(i+k>r){
-                l=i-k;
-                r=i+k;
-            }
-            if(k>ml){
-                c=i;
-                ml=k;
+        int ri=0, l=0;
+        int n=s.size();
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                //dp[i+1][j-1] represent the sub set of s[i...j] which is s[i+1.....j-1] is a palindrom
+                if (s[i] == s[j] && (j-i <=2 || dp[i+1][j-1])){
+                    dp[i][j] = true;// means s[i...j] is palindrom
+                    if (l < (j-i+1)) {
+                        ri = i;
+                        l = j-i+1;
+                    }
+                }
             }
         }
-        int st=(c-ml)/2;
-        return s.substr(st,ml);
+
+        return s.substr(ri, l);
     }
 };
